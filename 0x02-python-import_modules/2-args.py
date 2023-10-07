07c36cfad20a6d520201f6079b2a1c241653324a
Michyle <michyleramdass@gmail.com>
2023-10-07 03:04:00 -0700

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if__name__ == "__main__":
+if __name__ == "__main__":
     import sys
     argLen = len(sys.argv)
     if argLen == 1:
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, math
+    import sys
     total = 0
     for i in sys.argv[1:]:
         total += int(i)
-        print("{}".format(total))
+    print("{}".format(total))
