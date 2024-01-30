da0e2250fec6afed187eb2087e2a14e474e4f4bf
Michyle <michyleramdass@gmail.com>
2024-01-30 14:46:57 -0800

    push

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 #a Python script that fetches https://alx-intranet.hbtn.io/status
-import urllib.request
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
